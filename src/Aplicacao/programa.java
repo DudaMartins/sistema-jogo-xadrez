269884cc6b2bb7aa4f009e7cede5ec3e91dd48f4
DudaMartins <maria.gomes@dcx.ufpb.br>
2022-05-04 10:38:53 -0300

    First class : posicao

---
+++ src/Aplicacao/programa.java
@@ -0,0 +1,12 @@
+package Aplicacao;
+
+import tabuleiro.Posicao;
+
+public class programa {
+
+    public static void main(String[] args){
+
+        Posicao pos = new Posicao(3 , 5 );
+        System.out.println(pos);
+    }
+}
